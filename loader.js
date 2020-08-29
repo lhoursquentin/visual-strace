264420abb1f302dc3b60391d3139e69b661edb0b
Quentin L'Hours <lhoursquentin@gmail.com>
2020-08-29 21:43:09 +0200

    feat: disable run btn if input is empty

--- loader.js
+++ loader.js
@@ -1,18 +1,26 @@
 var speedSetupNeeded = true;
 const straceOutputElement = document.getElementById('strace-output');
+const runButton = document.getElementById('run');
 
 const loadStrace = () => {
   showGraph(straceOutputElement.value);
 };
 
 const loadExample = () => {
-  straceOutputElement.value = exampleStraceOutput;
   speedSetupNeeded = true;
+  straceOutputElement.value = exampleStraceOutput;
+  runButton.disabled = false;
   showGraph(exampleStraceOutput);
 };
 
-document.getElementById('run').onclick = loadStrace;
+runButton.onclick = loadStrace;
 document.getElementById('load-example').onclick = loadExample;
 // If we change the strace output, reset the speed default values on the next
 // run
-straceOutputElement.addEventListener('input', (() => speedSetupNeeded = true));
+straceOutputElement.addEventListener(
+  'input',
+  (event) => {
+    speedSetupNeeded = true;
+    runButton.disabled = event.target.value.trim() === '';
+  },
+);
