541295e8c95ac5b1cfe2554df7feefb295b5bd0a
Quentin L'Hours <lhoursquentin@gmail.com>
2020-08-30 16:13:54 +0200

    feat: add button to minimize/expand loader

--- loader.js
+++ loader.js
@@ -1,10 +1,11 @@
-var speedSetupNeeded = true;
-const straceOutputElement = document.getElementById('strace-output');
+const loadExampleButton = document.getElementById('load-example');
 const runButton = document.getElementById('run');
+const straceCmdInput = document.getElementById('strace-cmd');
+const straceOutputElement = document.getElementById('strace-output');
+const toggleLoaderButton = document.getElementById('toggle-loader');
+const loaderContent = document.getElementById('loader-content');
 
-const loadStrace = () => {
-  showGraph(straceOutputElement.value);
-};
+var speedSetupNeeded = true;
 
 const loadExample = () => {
   speedSetupNeeded = true;
@@ -13,15 +14,33 @@ const loadExample = () => {
   showGraph(exampleStraceOutput);
 };
 
-runButton.onclick = loadStrace;
-document.getElementById('load-example').onclick = loadExample;
-// If we change the strace output, reset the speed default values on the next
-// run
-straceOutputElement.addEventListener('input', (event) => {
-  speedSetupNeeded = true;
-  runButton.disabled = event.target.value.trim() === '';
-});
+const loadStrace = () => {
+  showGraph(straceOutputElement.value);
+};
 
-const straceCmdInput = document.getElementById('strace-cmd');
+const straceOutputChange = ({ target: { value } }) => {
+  // If we change the strace output, reset the speed default values on the next
+  // run
+  speedSetupNeeded = true;
+  runButton.disabled = value.trim() === '';
+};
+
+const toggleLoader = ({ target }) => {
+  if (loaderContent.style.display === 'none') {
+    target.title = 'minimize window';
+    target.innerText = '-';
+    loaderContent.style.display = 'block';
+  } else {
+    target.title = 'expand window';
+    target.innerText = '+';
+    loaderContent.style.display = 'none';
+  }
+};
+
+loadExampleButton.addEventListener('click', loadExample);
+runButton.addEventListener('click', loadStrace);
+straceOutputElement.addEventListener('input', straceOutputChange);
+toggleLoaderButton.addEventListener('click', toggleLoader);
+
 // Using input.size is clearer but is somehow slightly wider than the content
 straceCmdInput.style.width = `${straceCmdInput.value.length}ch`;
